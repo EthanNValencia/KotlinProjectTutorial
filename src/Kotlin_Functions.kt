38f3e960b40c764ebf16032a94a462ccb8583a7f
Ethan N <ejnephew@yahoo.com>
2021-05-12 20:59:48 -0400

    Collections.

--- src/Kotlin_Functions.kt
+++ src/Kotlin_Functions.kt
@@ -24,6 +24,15 @@ fun getOtherWord() = "Other word" /* this is a "Single Expression Function" that
                                      Kotlin uses type inference. It can tell what sort of information we are returning,
                                      so it is not necessary to always state the return type. */
 
+fun sayHi1(itemToGreet: String){ // this function takes a string and calls that string itemToGreet
+    val msg = "Hi $itemToGreet"  // it then concats the itemToGreet with Hi
+    println(msg) // it prints the contents of msg
+}
+
+fun sayHi2(itemToGreet: String) = println("Hello $itemToGreet") // this is shorthand notation of the sayHi() function.
+
+fun sayHi3(hello:String, there:String) = println("$hello $there") // this function takes 2 variable parameters.
+
 fun main(){
     println(getGreeting()) // this prints what the getGreeting method call returns.
     println(getNull())
@@ -33,4 +42,8 @@ fun main(){
 
     println(getWord())
     println(getOtherWord())
+
+    sayHi1("there") // this calls a function that takes a String and concats the String to a local variable.
+    sayHi2("Ethan")
+    sayHi3("Hello", "there")
 }
