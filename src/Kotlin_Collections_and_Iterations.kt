aaeee5c784d7f7f22633f9d12fa980662cd36c53
Ethan N <ejnephew@yahoo.com>
2021-05-12 21:24:22 -0400

    Collections.

--- src/Kotlin_Collections_and_Iterations.kt
+++ src/Kotlin_Collections_and_Iterations.kt
@@ -6,5 +6,14 @@ Iterations
  */
 
 fun main(){
-
+    val stuffArray = arrayOf("Stuffs", "Things", "Even more stuffs")
+    println(stuffArray.asList())
+    println(stuffArray.size)
+    println(stuffArray[0])
+    println(stuffArray.get(0))
+    println("----------------")
+    for (stuffArray in stuffArray){
+        println(stuffArray)
+    }
+    println("----------------")
 }
