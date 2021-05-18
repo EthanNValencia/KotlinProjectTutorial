430989fdd6fcc424e56715c31a0a82be2ce886a8
Ethan N <ejnephew@yahoo.com>
2021-05-18 16:26:57 -0400

    Working on Kotlin classes.

--- src/Kotlin_Person.kt
+++ src/Kotlin_Person.kt
@@ -3,8 +3,28 @@
 Topics covered here:
 Class
 
+If there are no properties defined in your primary constructor, then is okay to omit it. This is similar to Java.
  */
 
-class Person{
 
+
+
+class Person (val first_Name:String, val last_Name:String) { // two parameters that are required for a person class
+
+    /*
+    This is another way of assigning values to the object.
+    val firstName:String
+    val lastName:String
+
+    init { // the init block is run anytime this class is instantiated.
+        firstName = first_Name
+        lastName = last_Name
+    }
+    */
+
+    /*
+    Alternatively, the data values can be assigned using a more traditional Java approach.
+    val firstName:String = first_Name
+    val lastName:String = last_Name
+    */
 }
---
+++ src/Main.kt
@@ -0,0 +1,13 @@
+/*
+#5
+Topics covered here:
+Class (works with Kotlin_Person class file)
+
+ */
+
+
+
+fun main(){
+    val person = Person("Ethan", "Nephew") // no "new" keyword is used here.
+    println(person.first_Name + " " + person.last_Name) // no getter is being used here.
+}
