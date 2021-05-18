d94b905bdcccbebcf535da80a9450c801cd53050
Ethan N <ejnephew@yahoo.com>
2021-05-18 16:14:19 -0400

    Working on collections and iterations.

--- src/Kotlin_Collections_and_Iterations.kt
+++ src/Kotlin_Collections_and_Iterations.kt
@@ -9,11 +9,21 @@ this is similar to how Kotlin differentiates between null and non-null values.
  */
 
 
+
 fun helloFunction(hello:String, items:List<String>) {
     items.forEach { items ->
         println("$hello $items")
     }
 }
+
+// vararg variable number of arguments, this will be treated as an array of whatever type it is specified (String in this case).
+fun helloFunction(hello:String, vararg items:String) {
+    items.forEach { items ->
+        println("$hello $items")
+    }
+}
+
+fun greetPerson(greeting:String = "Hello", name:String = "Ethan") = println("$greeting $name") // Hello and Ethan are the default values, but they can be overided
 
 fun main(){
 
@@ -66,7 +76,23 @@ fun main(){
 
     println("----------------")
     println("-Function List--")
-    helloFunction("Hello", list)
+    helloFunction("Number:", list)
+    helloFunction("Empty") // nothing will be printed out, but this will not throw an error.
+    helloFunction("Hi", "Kotlin","Android","Programming") // All of these items will be grouped together and treated as an array.
+    val thingArray = arrayOf("Kotlin","Android","Programming")
+    helloFunction("Hello", *thingArray) // * is called the "spread operator", so I am passing in the array as a vararg parameter.
+    println("----------------")
+
+    println("---Specified----")
+    println("---Arguments----")
+    greetPerson() // uses the default greeting and name values.
+    greetPerson("Hello", "Ethan")
+    greetPerson(greeting = "Hello", name = "Ethan") // I am effectively leveraging default parameter values.
+    greetPerson(name = "Ethan", greeting = "Hello")
+    // The above lines have the same results.
+    // Tutorial video claims that when using specified argument syntax, all parameters must be named.
+    helloFunction(hello = "Hello", *thingArray)
+    // But the above line ^ works.
     println("----------------")
 }
 
