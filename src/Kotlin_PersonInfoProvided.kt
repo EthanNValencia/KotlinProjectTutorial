4bb49e6bb3939130f9d2afa1114ee3f001e79995
Ethan N <ejnephew@yahoo.com>
2021-05-19 13:34:53 -0400

    Working on Kotlin interfaces.

--- src/Kotlin_PersonInfoProvided.kt
+++ src/Kotlin_PersonInfoProvided.kt
@@ -19,8 +19,12 @@ interface Kotlin_PersonInfoProvided {
 
 }
 
+interface SessionInfoProvider{
+    fun getSessionId():String
+}
+
 // abstract class will allow me to implement an interface without having to use all it's functions.
-class BasicInfoProvider : Kotlin_PersonInfoProvided { // this class implements an interface
+class BasicInfoProvider : Kotlin_PersonInfoProvided, SessionInfoProvider { // this class implements an interface
 
     override val providerInfo:String
         get() = "Basic Information"
@@ -28,10 +32,29 @@ class BasicInfoProvider : Kotlin_PersonInfoProvided { // this class implements a
     //override fun printInfo(person: Person) { // Java has an override annotation, but in Kotlin the override is required.
 
     //}
+
+    override fun getSessionId(): String {
+        return "Session101"
+    }
+}
+/*
+This checks if the object implements SessionInfoProvider.
+By using !is I can determine if something "is not" something.
+*/
+fun checkType(infoProvider: Kotlin_PersonInfoProvided){
+    if(infoProvider is SessionInfoProvider){ // this checks if the object implements SessionInfoProvider
+        println("IP is SIP")
+        (infoProvider as SessionInfoProvider).getSessionId() // this casts infoProvider as a SessionInfoProvider
+    } else {
+        println("IP is not SIP")
+    }
 }
 
 fun main(){
     val provider = BasicInfoProvider()
 
     provider.printInfo(Person())
+    println(provider.getSessionId())
+
+    checkType(provider)
 }
