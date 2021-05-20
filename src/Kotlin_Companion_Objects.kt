9e64666cb848fe4724d5bd87fa4c738b9f30a955
Ethan N <ejnephew@yahoo.com>
2021-05-20 10:12:00 -0400

    Companion object.

--- src/Kotlin_Companion_Objects.kt
+++ src/Kotlin_Companion_Objects.kt
@@ -8,10 +8,26 @@ without explicitly declaring a new subclass for it. Kotlin handles this case
 with object expressions and object declarations.
 */
 
+interface IdProvider {
+    fun get(): String
+}
 
-class Entity(val id: String)
+
+class Entity private constructor(val id: String) {
+
+    companion object Factory: IdProvider{
+
+        fun create() = Entity(get())
+
+        override fun get(): String {
+            return "ID:124"
+        }
+    }
+}
 
 
 fun main(){
-    val entity = Entity("id")
+    val entity1 = Entity.Factory.create() // method 1 for calling the companion object
+    val entity2 = Entity.create() // method 2 for calling the companion object
+    println(entity1.id)
 }
